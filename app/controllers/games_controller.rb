c30dcde508b9ee77034029daa5eec251bf992644
olesya steinmetz <newsabome@icloud.com>
2020-05-18 18:16:59 +0200

    Longest word game

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,25 @@
+class GamesController < ApplicationController
+  ALPHABET = ('A'..'Z').to_a
+  def new
+  	@letters = 10.times.map { ALPHABET.sample }
+  end
+ 
+  def score
+    @letters = params[:letters].split
+    @word = (params[:word] || "").upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+  end
+
+  private
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
+  end
+end
